90aeeabf0c0c1ff994086f900c2f2c7ea9e98db0
peangoulvant@gmail.com <almalinux@almalinux9.linuxvmimages.local>
2025-02-09 17:18:55 +0100

    Création d'une surface + cube avec Three.js

--- react-3dviewer/src/App.tsx
+++ react-3dviewer/src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Edges, OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+
+
+function Plane() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+      <planeGeometry args={[10, 10]} />
+      <meshStandardMaterial color={'lightblue'} side={THREE.DoubleSide} />
+    </mesh>
+  );
+}
+
+function Cube() {
+  return (
+    <mesh position={[0, 0.5, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'orange'} />
+      <Edges>
+        <lineBasicMaterial color="black" />
+      </Edges>
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-card">
+      <h1>Test</h1>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Plane />
+        <Cube/>
+        <OrbitControls />
+      </Canvas>
     </div>
+
   );
 }
 
